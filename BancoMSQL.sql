0469349d2a17a02b48d1330fe7d5e0147f8d8435
Julia Paiva <juliapoliver12@gmail.com>
2023-12-06 15:48:50 -0300

    CRUD

--- BancoMSQL.sql
+++ BancoMSQL.sql
@@ -9,7 +9,13 @@ email varchar(255),
 );
 
 insert into clientes(nome, email, cpf) values 
-("JUlia paiva", "julia@gmail.com", "23823923828");
+("Julia", "julia@gmail.com", "123");
+("Loira", "loira@gmail.com", "234");
+("Larissa", "larissa@gmail.com", "345");
+("Igor", "igor@gmail.com", "456");
+("Calsolari", "calsolari@gmail.com", "567");
+("Pedro", "pedro@gmail.com", "678");
+("Kaique", "kaique@gmail.com", "789");
 
 
 create table produtos(
@@ -18,8 +24,16 @@ nome varchar(255),
 quantidade int,
 preco integer,
 descricao varchar(555),
-atributo varchar(255)
+atributo varchar(255),
+fornecedor varchar(255)
 );
+insert into clientes(nome, descricao, atributo, fornecedor, quantidade, preco) values
+("mouse", "Geek", "sem fio", "CCXS", 12, 48)
+("teclado", "rgb", "com fio", "CCXS", 12, 97)
+("monitor", "hd", "led", "FPg", 22, 128)
+("notebook", "Pequeno portatil", "hd", "RRXS", 2, 2098)
+("pendrive", "perifericos", "wireless", "RRXS", 672, 68)
+("fonte", "Carregador notebook GP6", "de fio", "EletornicS", 451, 128)
 
 create table compra(
 id integer not null auto_increment primary key,
